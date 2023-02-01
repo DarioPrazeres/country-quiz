28cb8f5ebd65fa0b5fabcf79b04b266b827412f1
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-02-01 01:11:17 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -31,22 +31,4 @@ function App() {
 }
 export { ContContext }
 
-/*
-function shuffle(array) {
-  var tmp, current, top = array.length;
-  if(top) while(--top) {
-    current = Math.floor(Math.random() * (top + 1));
-    tmp = array[current];
-    array[current] = array[top];
-    array[top] = tmp;
-  }
-  return array;
-}
-
-function numberRandom(){
-  return Math.floor(Math.random()*4);
-}
-*/
-
-
 export default App;
