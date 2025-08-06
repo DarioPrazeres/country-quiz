73116b08a3ff7b457ac2ce2742e6344c46d4213f
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2025-08-06 23:25:55 +0100

    feat internalization

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-    return (
-        <footer>
-            <p>Created by <a href="https://github.com/DarioPrazeres">Dario Prazeres</a> - <a href="https://github.com/DarioPrazeres">DP Project</a></p> 
-        </footer>
-    )
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+  const userName = "Dário";
+  return (
+    <footer>
+      <div>
+        <h1>{t("welcome", { name: userName })}</h1>
+        <button onClick={() => changeLanguage("en")}>English</button>
+        <button onClick={() => changeLanguage("pt")}>Português</button>
+      </div>
+      <p>
+        Created by <a href="https://github.com/DarioPrazeres">Dario Prazeres</a>{" "}
+        - <a href="https://github.com/DarioPrazeres">DP Project</a>
+      </p>
+    </footer>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import Footer from './component/Footer';
+import './component/utils/i18n'; 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
