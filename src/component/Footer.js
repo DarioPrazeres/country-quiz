d8e3c033bff79a6e8d594c68b6f91e9c4dedcf2a
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2025-08-08 21:51:16 +0100

    create a scheme to setting languange tranlation

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,21 +2,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-  const { t, i18n } = useTranslation();
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-  const userName = "Dário";
+  const { t } = useTranslation();
+  const linkGitHub = "https://github.com/DarioPrazeres";
   return (
     <footer>
-      <div>
-        <h1>{t("welcome", { name: userName })}</h1>
-        <button onClick={() => changeLanguage("en")}>English</button>
-        <button onClick={() => changeLanguage("pt")}>Português</button>
-      </div>
       <p>
-        Created by <a href="https://github.com/DarioPrazeres">Dario Prazeres</a>{" "}
-        - <a href="https://github.com/DarioPrazeres">DP Project</a>
+        {t("create_by")}<a href={{linkGitHub}}>Dario Prazeres</a>{" "}
+        - <a href={{linkGitHub}}>DP Project</a>
       </p>
     </footer>
   );
