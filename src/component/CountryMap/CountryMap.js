3a62d0c0d7b38f80b0e121eff00e857c08120e8a
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2025-09-13 20:09:24 +0100

    remove all cometaries

--- src/component/CountryMap/CountryMap.js
+++ src/component/CountryMap/CountryMap.js
@@ -3,7 +3,6 @@ import { MapContainer, TileLayer, Marker, GeoJSON, useMap } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Corrige o problema do ícone do Marker
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
@@ -16,7 +15,6 @@ const defaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = defaultIcon;
 
-// Componente auxiliar para mudar a visão do mapa sempre que latlng mudar
 function RecenterMap({ latlng, zoom = 5 }) {
   const map = useMap();
   useEffect(() => {
@@ -31,7 +29,6 @@ function CountryMap({ latlng, name }) {
   const [countries, setCountries] = useState(null);
 
   useEffect(() => {
-    // Carrega GeoJSON com fronteiras dos países
     fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
       .then((res) => res.json())
       .then((data) => setCountries(data));
@@ -43,28 +40,24 @@ function CountryMap({ latlng, name }) {
       zoom={5}
       style={{ height: "300px", width: "400px" }}
     >
-      {/* Fundo sem labels */}
       <TileLayer
         url="https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}{r}.png"
         attribution='&copy; OpenStreetMap &copy; Carto'
       />
 
-      {/* Fronteiras dos países */}
       {countries && (
         <GeoJSON
           data={countries}
           style={() => ({
-            color: "black",   // cor da borda
-            weight: 1,        // espessura
-            fillOpacity: 0    // sem preenchimento
+            color: "black",   
+            weight: 1,        
+            fillOpacity: 0   
           })}
         />
       )}
 
-      {/* Marcador */}
       <Marker position={latlng} />
 
-      {/* Recentrar o mapa sempre que latlng mudar */}
       <RecenterMap latlng={latlng} zoom={5} />
     </MapContainer>
   );
